d9dbc55902bce5cac65bf51808b7255bfc476381
Alain Somet <sometalain@yahoo.fr>
2018-12-10 14:10:49 +0100

    solution quetes

---
+++ app/src/main/java/fr/wildcodeschool/photoapplication/MainActivity.java
@@ -0,0 +1,105 @@
+package fr.wildcodeschool.photoapplication;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int REQUEST_IMAGE_CAPTURE = 1234;
+    ImageView mImageView;
+    private Uri mFileUri = null;
+    String mCurrentPhotoPath;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button photoButton = (Button) this.findViewById(R.id.button);
+        photoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+
+            }
+
+        });
+    }
+
+    private void dispatchTakePictureIntent() {
+
+        // ouvrir l'application de prise de photo
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // lors de la validation de la photo
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // créer le fichier contenant la photo
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException e) {
+                // TODO : gérer l'erreur
+            }
+
+            if (photoFile != null) {
+                // récupèrer le chemin de la photo
+                mFileUri = FileProvider.getUriForFile(this,
+                        "fr.wildcodeschool.photoapplication.fileprovider",
+                        photoFile);
+                // déclenche l'appel de onActivityResult
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, mFileUri);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        ImageView mImageView = findViewById(R.id.imageView);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            mImageView.setImageURI(mFileUri);
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imgFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imgFileName, ".jpg", storageDir);
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+
+    }
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+    public static void addImageToGallery(final String filePath, final Context context) {
+
+        ContentValues values = new ContentValues();
+
+        values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        values.put(MediaStore.MediaColumns.DATA, filePath);
+
+        context.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+    }
+
+}
